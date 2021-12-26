8c0d68fd4934a28aed0898494614d2e07767987b
Jarak-Jakar <Jarak-Jakar@users.noreply.github.com>
2021-12-26 17:51:00 +1300

    Set up git tracking on the Fortran exercises

---
+++ Textbooks/Modern_Fortran_2021/Ch1/listing1dot3.f90
@@ -0,0 +1,20 @@
+program array_copy_caf
+    
+    implicit none
+
+    integer :: array(5)[*] = 0
+    integer, parameter :: sender = 1, receiver = 2
+
+    if (num_images() /= 2) stop 'Error: This program must be run on 2 parallel processes'
+
+    if (this_image() == sender) array = [1, 2, 3, 4, 5]
+
+    print '(a,i1,a,5(4x,i2))', 'array on proc ', this_image(), ' before copy: ', array
+
+    sync all
+
+    if (this_image() == receiver) array(:) = array(:)[sender]
+
+    print '(a,i1,a,5(4x,i2))', 'array on proc ', this_image(), ' after copy: ', array
+
+end program array_copy_caf
