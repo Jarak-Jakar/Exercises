d401739c9c8baab0f4f55ce43faef05b5d5ca8d2
Jarak-Jakar <Jarak-Jakar@users.noreply.github.com>
2022-09-18 15:56:54 +1200

    Continue working on the doubly-linked list.  It actually kinda works, now.

--- Textbooks/Crafting_Interpreters/Chapter One/DoublyLinkedList.c
+++ Textbooks/Crafting_Interpreters/Chapter One/DoublyLinkedList.c
@@ -1,12 +1,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-typedef struct DoublyLinkedList
-{
-    struct Node *Front;
-    struct Node *Back;
-} DoublyLinkedList;
+#include <stdbool.h>
 
 typedef struct Node
 {
@@ -15,23 +10,119 @@ typedef struct Node
     char *String;
 } Node;
 
-DoublyLinkedList *NewList(char *initialStr)
+typedef struct DoublyLinkedList
 {
-    Node frontNode = {.String = initialStr};
+    struct Node *Front;
+    struct Node *Back;
+} DoublyLinkedList;
+
+DoublyLinkedList *New(char *initialStr)
+{
+    Node *frontNode = calloc(1, sizeof(Node));
+    frontNode->Next = NULL;
+    frontNode->Prev = NULL;
+    frontNode->String = initialStr;
+
     DoublyLinkedList *ptr = calloc(1, sizeof(DoublyLinkedList));
-    DoublyLinkedList dll = {.Front = &frontNode, .Back = &frontNode};
-    *ptr = dll;
+    ptr->Front = frontNode;
+    ptr->Back = frontNode;
+
     return ptr;
 }
 
-void AppendList(DoublyLinkedList *list, char *string)
+void Append(DoublyLinkedList *list, char *string)
 {
     Node *backNode = list->Back;
-    Node newNode = {.Next = NULL, .Prev = backNode, .String = string};
-    backNode->Next = &newNode;
+    Node *newNode = calloc(1, (sizeof(Node)));
+    newNode->Next = NULL;
+    newNode->Prev = backNode;
+    newNode->String = string;
+    backNode->Next = newNode;
+    list->Back = newNode;
+}
+
+void Prepend(DoublyLinkedList *list, char *string)
+{
+    Node *frontNode = list->Front;
+    Node *newNode = calloc(1, sizeof(Node));
+    newNode->Next = frontNode;
+    newNode->Prev = NULL;
+    newNode->String = string;
+    frontNode->Prev = newNode;
+    list->Front = newNode;
+}
+
+void PrintStringsForward(DoublyLinkedList *list)
+{
+    printf("\n"); // A further goal would be to avoid printing this if the list is empty...
+    Node *node = list->Front;
+    while (node != NULL)
+    {
+        printf("%s ", node->String);
+        node = node->Next;
+    }
+    printf("\n");
+}
+
+void PrintStringsBackward(DoublyLinkedList *list)
+{
+    printf("\n");
+    Node *node = list->Back;
+    while (node != NULL)
+    {
+        printf("%s ", node->String);
+        node = node->Prev;
+    }
+    printf("\n");
+}
+
+// Returns true if the node  string was inserted, or false if it couldn't be for whatever reason.
+// This assumes zero-based indexing
+bool InsertAt(DoublyLinkedList *list, char *string, int index)
+{
+
+    if (index == 0)
+    {
+        Prepend(list, string);
+        return true;
+    }
+
+    Node *node = list->Front;
+    int idx = 1;
+    bool success = false;
+
+    while (node != NULL)
+    {
+        if (idx < index)
+        {
+            node = node->Next;
+            idx++;
+        }
+        else
+        {
+            Node *newNode = calloc(1, sizeof(Node));
+            newNode->Next = node->Next;
+            newNode->Prev = node;
+            newNode->String = string;
+            node->Next = newNode;
+            success = true;
+            break;
+        }
+    }
+
+    return success;
 }
 
 int main()
 {
-    return printf("\nHello World!\n\n");
+    DoublyLinkedList *dll = New("Hello");
+    PrintStringsForward(dll);
+    Append(dll, "there");
+    PrintStringsForward(dll);
+    Append(dll, "children!");
+    PrintStringsForward(dll);
+    PrintStringsBackward(dll);
+
+    InsertAt(dll, "rambunctious", 1);
+    PrintStringsForward(dll);
 }
